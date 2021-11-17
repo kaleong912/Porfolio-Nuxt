5e95eab09058ce7f9bfd137db9a458a3c866b85e
Ka Leong Chow <kaleong@nuthon.com>
2021-11-17 18:32:08 +0800

    initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import './assets/css/tailwind.css'
+</script>
+
+<template>
+<div class="container mx-auto flex p-4 justify-center">
+   <h1 class="font-semibold">Hello world! I am App.vue</h1>
+</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="p-4">
+        <h1 class="font-semibold">Hello world! I am done</h1>
+    </div>
+</template>
